d12eac6de785b8280cd5c4bc1d775bdd0b766895
abdullah <69767720+Abdullah-Bajwa@users.noreply.github.com>
2024-09-11 19:16:54 +0500

    init

---
+++ public/src/CustomJs.js
@@ -0,0 +1,6 @@
+window.myFloorplan = myFloorplan;
+window.furniturePalette = furniturePalette;
+window.wallPartsPalette = wallPartsPalette;
+function saveFloorPlan() {
+  console.log(myFloorplan.model.De);
+}
